82fd1fe274e00acad76414919094ed1c106d1a8d
programer1983 <programer972@gmail.com>
2022-04-21 15:48:09 +0300

    commit-4;

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,51 @@
+import { Close, MenuOutlined } from "@mui/icons-material"
+import {useState} from "react"
+import {Link} from "react-router-dom"
 import "./styles/Header.scss"
 
 
 const Header = () => {
+  const [active, setActive] = useState(false)
+
+  const showMenu = () => {
+    setActive(!active)
+  }
+
+
   return (
-    <div>Header</div>
+    <div className="header">
+        <div className="header__logo">
+            <h1>Leedd</h1>
+        </div>
+        <nav className={active ? "navbar active" : "navbar"}>
+            <ul>
+                <div className="closed">
+                    <Close className="close" onClick={showMenu} />
+                </div>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/">About</Link>
+                </li>
+                <li>
+                    <Link to="/">Portfolio</Link>
+                </li>
+                <li>
+                    <Link to="/">Pages</Link>
+                </li>
+                <li>
+                    <Link to="/">Blog</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
+        </nav>
+        <div className="changer">
+            <MenuOutlined className="menu" onClick={showMenu} />
+        </div>
+    </div>
   )
 }
 
