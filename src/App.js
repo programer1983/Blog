0c11433b5684678473af9895714ad7f5fe6f7822
programer1983 <programer972@gmail.com>
2022-04-22 15:49:26 +0300

    commit-7;

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import Header from './components/Header';
 import Main from './components/Main';
+import Section from './components/Section';
 
 function App() {
   return (
     <div className="App">
        <Header />
        <Main />
+       <Section />
     </div>
   );
 }
